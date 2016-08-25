50c261b7141549b4087cd5042d2e995292b621c9
imjoohyu <joohyun.im@gmail.com>
2016-08-25 17:23:32 -0400

    Added longest isoform discovery feature and created a simple command to make a .sh script file for downloading gene sequences

--- Pulling_out_Dsim_sequences.R
+++ Pulling_out_Dsim_sequences.R
@@ -1,6 +1,8 @@
+####################################################
 #Pulling out Dsim CDS sequences from WGS
 #July 22nd, 2016
 #Joo Hyun Im (ji72)
+####################################################
 
 #delete any previous input
 rm(list=ls(all=TRUE))
@@ -24,18 +26,21 @@ int.genes.total = cbind(int.genes, output.mx); colnames(int.genes.total) = c("FB
 write.table(int.genes.total, file ="../list.of.case.and.cont.genes.in.Dmel.FBgn.and.CG.txt", quote=F, row.names = F)
 #There were several genes that didn't get the right names (listed as 'NA's). I manually filled them in.
 
+
+############################################################################################################################################################
 #2. Read in GFF file and only get the CDS information
 dsim.full.gff = read.table("gff/dsim_update.final_cg_cleaned_up.gff.txt")
 dsim.cds.gff <- unique(dsim.full.gff[which(dsim.full.gff$V3== 'CDS'),]) 
-int.genes.cg = read.table("../list.of.case.and.cont.genes.in.Dmel.FBgn.and.CG.txt", header = T) #I changed 'NA's to right names.
+int.genes.cg = read.table("information_files/list.of.case.and.cont.genes.FBgn.and.CG.for.Dsim.final.txt", header = T) #I changed 'NA's to right names.
 list <- read.table("fly_line_list.txt")
 
 missing.genes = c()
 wgs.coordinates = matrix(nrow=dim(int.genes.cg)[1], ncol=7); colnames(wgs.coordinates) = c("FBgn.number", "gene.name","CG.name","chr", "strand","start.coordinate","end.coordinate")
 for (i in 1:dim(int.genes.cg)[1]){
     #Look for the CG name of a gene in the GFF file
-    gene.of.interest = as.character(paste(int.genes.cg[i,3],"-",sep="")); #print(gene.of.interest)
+    gene.of.interest = as.character(paste(int.genes.cg[i,4],"-",sep="")); #8/25/16, I changed [i,3] to [i,4] to reflect the CG names that include the initially missing genes.
     dsim.cds.gff.gene.specific = dsim.cds.gff[grep(gene.of.interest, dsim.cds.gff$V9),]
+    
     if (dim(dsim.cds.gff.gene.specific)[1] == 0){ #If the gene is missing in the GFF file,
         missing.genes = rbind(missing.genes, gene.of.interest)
         wgs.coordinates[i,1] = as.character(int.genes.cg[i,1]); wgs.coordinates[i,2] = as.character(int.genes.cg[i,2]); wgs.coordinates[i,3] = as.character(int.genes.cg[i,3]); wgs.coordinates[i,4] = "NA"; wgs.coordinates[i,5] = "NA"; wgs.coordinates[i,6] = "NA"; wgs.coordinates[i,7] = "NA";
@@ -45,9 +50,22 @@ for (i in 1:dim(int.genes.cg)[1]){
     #The missing ones are all from different chromosomes (some from 3R, others from X, etc.)
     #For 103 missing genes, saved in missing.genes, I used DsimVSDmel.orthos to find the unconventional name for
     #the CG name (ex. 6.g309.t1 is CG4944) and updated the "list.of.case.and.cont.genes.in.Dmel.FBgn.and.CG" to have different names for missing genes.
-    
+    #This was finished on 8/14/2016 (check Evernote)
     
     else { #If the gene is in the GFF file,
+        
+        #Check if there's more than one isoform (added on 8/25/2016)
+        dsim.cds.gff.gene.specific$V9 = factor(dsim.cds.gff.gene.specific$V9)
+        if (length(levels(dsim.cds.gff.gene.specific$V9))>1){#more than one isoform
+            max=c(); chosen.isoform=c()
+            for (p in 1:length(levels(dsim.cds.gff.gene.specific$V9))){
+                isoform = dsim.cds.gff.gene.specific[which(dsim.cds.gff.gene.specific$V9 == levels(dsim.cds.gff.gene.specific$V9)[p]),]
+                max=c(max,isoform[as.numeric(dim(isoform)[1]),5])
+            }
+            chosen.isoform = levels(dsim.cds.gff.gene.specific$V9)[which.max(max)]
+            dsim.cds.gff.gene.specific = dsim.cds.gff.gene.specific[which(dsim.cds.gff.gene.specific$V9 == chosen.isoform),]
+        }
+        
         if (dsim.cds.gff.gene.specific$V7 == '+'){ #plus strand
             gene.c1 <- as.numeric(dsim.cds.gff.gene.specific[1,4]) -1
             gene.c2 <- as.numeric(dsim.cds.gff.gene.specific[dim(dsim.cds.gff.gene.specific)[1],5]) -1
@@ -106,7 +124,7 @@ for (i in 1:dim(int.genes.cg)[1]){
             specific.gene <- matrix(rep(t(coordinates.df.mx),21), ncol=2, byrow=T)
             list.multi <- rep(as.matrix(list), each=dim(coordinates.df.mx)[1]) #or the number of CDS regions
             specific.gene.list <-cbind(list.multi,specific.gene,list.multi)
-            #write.table(specific.gene.list, file = paste("bed_files/",int.genes.cg[i,1],"_",int.genes.cg[i,2],"_",int.genes.cg[i,3],".bed", sep=""), quote=F, col.names= F, row.names=F, sep = "\t")
+            write.table(specific.gene.list, file = paste("bed_files/",int.genes.cg[i,1],"_",int.genes.cg[i,2],"_",int.genes.cg[i,3],".bed", sep=""), quote=F, col.names= F, row.names=F, sep = "\t")
         }
         
         else { #minus strand
@@ -183,11 +201,21 @@ for (i in 1:dim(int.genes.cg)[1]){
             specific.gene.list <-cbind(list.multi,specific.gene,list.multi)
             #print(specific.gene.list)
             
-            #write.table(specific.gene.list, file = paste("bed_files/",int.genes.cg[i,1], "_", int.genes.cg[i,2], "_", int.genes.cg[i,3],".bed", sep=""), quote=F, col.names= F, row.names=F, sep = "\t")
+            write.table(specific.gene.list, file = paste("bed_files/",int.genes.cg[i,1], "_", int.genes.cg[i,2], "_", int.genes.cg[i,3],".bed", sep=""), quote=F, col.names= F, row.names=F, sep = "\t")
         }
     }
     
 }
-#write.table(wgs.coordinates, file="wgs.coordinates.from.Dsim.txt", quote=F, row.names = F)
+write.table(wgs.coordinates, file="wgs.coordinates.from.Dsim.txt", quote=F, row.names = F)
+
+
+#3.Create a sh script to pull out Dsim sequences (pseudoCDS) -- added on 8/25/2016
+wgs.coordinates = data.frame(wgs.coordinates); wgs.coordinates.na.omit = wgs.coordinates[which(wgs.coordinates$chr != "NA"),] #Get rid of genes whose coordinates cannot be found
+sh = c(rep("sh loopGetSeqs.sh",each=as.numeric(dim(wgs.coordinates.na.omit)[1]))); symbol = c(rep(">", each = as.numeric(dim(wgs.coordinates.na.omit)[1])));cg.name.with.fa = paste(wgs.coordinates.na.omit$CG.name,".wgs.fa",sep=""); 
+wgs.coordinates.na.omit$chr = as.numeric(wgs.coordinates.na.omit$chr); wgs.coordinates.na.omit$start.coordinate = as.character(wgs.coordinates.na.omit$start.coordinate)
+wgs.coordinates.na.omit$end.coordinate = as.character(wgs.coordinates.na.omit$end.coordinate); wgs.coordinates.na.omit$CG.name = as.character(wgs.coordinates.na.omit$CG.name)
+script = data.frame(cbind(sh, wgs.coordinates.na.omit$chr, wgs.coordinates.na.omit$start.coordinate, wgs.coordinates.na.omit$end.coordinate, wgs.coordinates.na.omit$CG.name, symbol, cg.name.with.fa))
+write.table(script, file="loopGetSeqs_for_all_Dsim_genes.sh", quote=F, row.names = F, col.names = F)
+
 
 
